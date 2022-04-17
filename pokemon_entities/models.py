1a709375d281708dcc68d38c70fa5274a26ab466
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-17 14:09:36 +0500

    show Pokemon name in admin UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,3 +5,6 @@ class Pokemon(models.Model):
     """Покемон"""
     id = models.AutoField(auto_created=True, primary_key=True)
     title = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'{self.title}'
