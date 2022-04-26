c559d595caa3fca8f05cf7417c4beb7774f0c866
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-26 13:07:30 +0500

    del default field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,9 +3,6 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     """Покемон"""
-    id = models.AutoField(
-        auto_created=True,
-        primary_key=True)
     title_ru = models.CharField(
         max_length=200,
         verbose_name='Русское название')
